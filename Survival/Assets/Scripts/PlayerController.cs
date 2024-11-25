3700bb663db8edd346b537e16ade8e9b7372f8cf
Dehydrogenase <michalrodak98@gmail.com>
2024-11-25 20:21:12 +0100

    Movement wasd - input system

--- Survival/Assets/Scripts/PlayerController.cs
+++ Survival/Assets/Scripts/PlayerController.cs
@@ -3,12 +3,30 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private float moveSpeed;
+    private Vector2 currentMovementInput;
+
+    [Header("Look")]
     public Transform cameraContainer;
     public float minXLook;
     public float maxXLook;
     private float camCurrentXRotation;
     public float lookSensitivity;
     private Vector2 mouseDelta;
+
+    //components
+    private Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
 
     private void Start()
     {
@@ -18,6 +36,15 @@ public class PlayerController : MonoBehaviour
     private void LateUpdate()
     {
         CameraLook();
+    }
+
+    private void Move()
+    {
+        Vector3 direction = transform.forward * currentMovementInput.y + transform.right * currentMovementInput.x;
+        direction *= moveSpeed;
+        direction.y = rigidbody.linearVelocity.y;
+
+        rigidbody.linearVelocity = direction;
     }
 
     //looking around with camera
@@ -30,9 +57,22 @@ public class PlayerController : MonoBehaviour
         transform.eulerAngles += new Vector3(0, mouseDelta.x * lookSensitivity, 0);
     }
 
-
+    //called when move mouse - input system
     public void OnLookInput (InputAction.CallbackContext context)
     {
         mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    public void OnMoveInput (InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            currentMovementInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            currentMovementInput = Vector2.zero;
+        }
+            { }
     }
 }
